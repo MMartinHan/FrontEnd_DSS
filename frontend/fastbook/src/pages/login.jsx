5c0542c45e41a1f5d57a1532ded049ed6ad576d8
MMartinHan <mmhan@espe.edu.ec>
2024-03-05 00:05:03 -0500

    login

---
+++ frontend/fastbook/src/pages/login.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../css/login.css";
+import Logo from "../assets/logo1.jpeg";
+
+const Login = () => {
+    return(
+    <div>
+        <div className="login__logo">
+            <img src={Logo} />
+        </div>
+        <div className="container">
+            <div className="login">
+                <h1 className="login__title">Sign In</h1>
+                <div className="login__group">
+                    <input type="text" required className="login__group__input" />
+                    <label className="login__group__label">Username</label>
+                </div>
+                <div className="login__group">
+                    <input type="password" required className="login__group__input" />
+                    <label className="login__group__label">Password</label>
+                </div>
+                <button className="login__sign-in">Sign In</button>
+            </div>
+        </div>
+    </div>);
+}
+
+export default Login;
