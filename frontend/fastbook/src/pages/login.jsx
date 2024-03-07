9429f9d5902c956f6d3dd216fca7a5b327ae23b9
MMartinHan <mmhan@espe.edu.ec>
2024-03-07 09:07:49 -0500

    Update

--- frontend/fastbook/src/pages/login.jsx
+++ frontend/fastbook/src/pages/login.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../css/login.css";
 import Logo from "../assets/logo1.jpeg";
 import Header from "../components/header";
 
 const Login = () => {
+
+    const URL = 'http://localhost:8000/admin/login/userdb/';
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const Username = 'mateo';
+    const Password = 'mateo123';
+
+    const handleData = async () => {
+        try {
+            const response = await fetch(URL);
+            const data = await response.json();
+            console.log(data);
+            // Manejar la respuesta del servidor según el resultado de la autenticación
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    const handleLoginClick = () => {
+        // Llamar a la función handleData para realizar la comparación
+        handleData();
+    };
+
+
     return(
     <div>   
         <div className="login__logo">
@@ -13,14 +37,12 @@ const Login = () => {
             <div className="login">
                 <h1 className="login__title">Sign In</h1>
                 <div className="login__group">
-                    <input type="text" required className="login__group__input" />
-                    <label className="login__group__label">Username</label>
+                    <input type="email" id="email" required className="login__group__input" placeholder="Email"/>
                 </div>
                 <div className="login__group">
-                    <input type="password" required className="login__group__input" />
-                    <label className="login__group__label">Password</label>
+                    <input type="password" id="password" required className="login__group__input" placeholder="Password" />
                 </div>
-                <button className="login__sign-in">Sign In</button>
+                <button className="login__sign-in" onClick={handleLoginClick}>Sign In</button>
             </div>
         </div>
     </div>);
